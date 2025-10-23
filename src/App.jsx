9637a233f7b67516999ac26a9070f00da4117b73
Mehrab-Abir <mjabir@myseneca.ca>
2025-10-23 14:10:48 -0400

    Project set-up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600">Hello From App ---</h1>
+    </>
+  )
+}
+
+export default App
